8edcb04634f502fc564be3e5aee84d4f0f1dd9ff
Pascal <pascaldevweb@gmail.com>
2022-06-08 16:31:18 +0200

    deuxième contribution pascal

---
+++ controllers/indexCtrl.php
@@ -0,0 +1,34 @@
+<?php
+
+if ($_POST['submit']){
+    foreach ($_SESSION['newComment'] as $value) {
+       echo '<div class ="avis">';
+       
+        foreach ($value as $key =>$val) {
+        if($key=="name"){
+            echo "<div class='$key'>Nom: $val</div>";
+        }
+        if($key=="firstname"){
+            echo "<div class='$key'>Prénom: $val</div>";
+        }
+        if($key=="mail"){
+            echo "<div class='$key'>Mail: $val</div>";
+        }
+        if($key=="facture"){
+            echo "<div class='$key'>Facture: $val</div>";
+        }
+        if($key=="comment"){
+            echo "<div class='$key'>Commentaire: $val</div>";
+        }
+        if($key=="note"){
+            echo "<div class='$key'>Note: $val</div>";
+        }
+    
+    
+    
+        }
+        echo '</div>';
+     }
+    }
+
+    ?>
---
+++ models/user.php
@@ -0,0 +1,61 @@
+<?php
+
+session_start();
+
+class User {
+
+    function __construct($firstname,$name,$mail,$facture,$comment,$note)
+    {
+        $this->firstname = $firstname;
+        $this->name = $name;
+        $this->mail = $mail;
+        $this->facture = $facture;
+        $this->comment = $comment;
+        $this->note = $note;
+    }
+
+    public function getFirstname(){
+        return $this->firstname;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getMail(){
+        return $this->comment;
+    }
+
+    public function getFacture(){
+        return $this->comment;
+    }
+
+    public function getComment(){
+        return $this->comment;
+    }
+
+    public function getNote(){
+        return $this->note;
+    }
+
+}
+
+
+if (isset($_POST['submit'])){
+    if (!isset($_SESSION['newComment'])){
+
+        $_SESSION['newComment']=[];
+    }
+    array_push($_SESSION['newComment'],new User($_POST['name'],$_POST['firstname'],$_POST['mail'],$_POST['facture'],$_POST['comment'],$_POST['note']));
+    
+
+ }
+
+ 
+
+
+
+
+ 
+
+?>
