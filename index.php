ddbf66f64058ba2ffd649646f507336c8328bb09
RahimLog <abderrahmane.sitayeb@gmail.com>
2022-06-10 09:11:39 +0200

    troisieme contribution

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 require 'models/User.php';
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +11,7 @@ require 'models/User.php';
 	<link rel="stylesheet" href="assets/CSS/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body class="bodyIndex">
 
 <div class="topnav">
@@ -24,20 +23,18 @@ require 'models/User.php';
 </div>
 <!-- Load icon library -->
 
-
 <!-- The form -->
 <form class="example" action="action_page.php">
   <input type="text" placeholder="Search.." name="search">
   <button type="submit"><i class="fa fa-search"></i></button>
 </form>
 
-	<h1>Bistrot de la Gare</h1>
-	<p>12 Rue Gentil Bernard, 92260 Fontenay-aux-Roses</p>
-	<p>Prix moyen 28 €</p>
-	<p>-20% sur la carte </p>
+<h1>Bistrot de la Gare</h1>
+<p>12 Rue Gentil Bernard, 92260 Fontenay-aux-Roses</p>
+<p>Prix moyen 28 €</p>
+<p>-20% sur la carte </p>
 
-
-	<!-- Slideshow container -->
+<!-- Slideshow container -->
 <div class="slideshow-container">
 
 <!-- Full-width images with number and caption text -->
@@ -72,16 +69,43 @@ require 'models/User.php';
 <span class="dot" onclick="currentSlide(3)"></span>
 </div>
 
+<div class="noteMoyenne">
+  <?php
+  if(isset($_SESSION['noteMoyenne'])){
+    echo  "La note moyenne du restaurant est : " . $_SESSION['noteMoyenne']. " .  le nombre d'avis est : ".sizeof($_SESSION['newNote']);
+  }
+     
+  ?> 
+
+</div>
 
 <div class="listAvis">
 <?php
-include_once "controllers/indexCtrl.php";
-?>
+  include_once "controllers/indexCtrl.php";
+  
+  ?>
 </div>
-
-
-<button class="buttonAvis"><a href="formulaire.php">Donner mon avis</a> </button>
-
+<div class="buttonAvis">
+  <button ><a href="formulaire.php">Donner mon avis</a> </button>
+</div>
+  
+<footer>
+  <div class="footer-link">
+    <div>
+      <p><a href="">Home</a></p>
+      <p><a href="">Contact</a></p>  
+      <p><a href="">Menu</a></p>    
+    </div>
+    <div>
+      <p><a href="">A propos</a></p> 
+      <p><a href="">Reservation</a></p>   
+      <p><a href="">Actualite</a></p>  
+    </div>
+  </div>
+  <div>
+    <p>© Bistrot de la Gare</p>
+  </div>
+</footer>
 <script src="assets/JS/script.js"></script>
 </body>
 </html>
